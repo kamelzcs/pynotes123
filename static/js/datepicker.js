8623c5e66e4bc91ce82b139157f3490ff610fb87
kamelzcs <kamelzcs@gmail.com>
2014-01-13 22:33:39 +0900

    Changed to Calendar.

--- static/js/datepicker.js
+++ static/js/datepicker.js
@@ -1,9 +1,11 @@
+$(function() {
 $("#datepicker").datepicker({
    onSelect: function(dateText, inst) { 
       var dateAsString = dateText; //the first parameter of this function
       var dateAsObject = $(this).datepicker( 'getDate' ); //the getDate method
+      $("#form input[name=year]").val(dateAsObject.getFullYear());
+      $("#form input[name=month]").val(dateAsObject.getMonth()+1);
+      $("#form input[name=day]").val(dateAsObject.getDate());
    }
 });
-$(function() {
-$( "#datepicker" ).datepicker();
 });
