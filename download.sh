94ad5e17f108522c25a7ce777fa483af79d7eb5b
kamelzcs <kamelzcs@gmail.com>
2014-06-19 13:05:04 +0900

    download data

---
+++ download.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# download.sh
+# Copyright (C) 2014 luxeys <info@luxeys.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+appcfg.py download_data --config_file=bulkloader.yaml --filename=shi.out --kind=Note --url=http://pynotes123.appspot.com/_ah/remote_api 
